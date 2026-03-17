f5ab5b9e6a6ff10419fee60bbd95dc70f837cc01
ymd <yinmingdi@max-optics.com>
2026-03-17 22:54:08 +0800

    fix: ci

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vitest/config";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
+  plugins: [vueJsx()],
   test: {
     environment: "jsdom",
     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
